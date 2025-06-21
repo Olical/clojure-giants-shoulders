8d4762dc31145e3c5149a7072e5a3a42337de9d6
Oliver Caldwell <ollie@oli.me.uk>
2025-06-21 13:44:46 +0100

    t over tc and an example bad call

--- src/example/core.clj
+++ src/example/core.clj
@@ -1,8 +1,11 @@
 (ns ^:typed.clojure example.core
-  (:require [typed.clojure :as tc]))
+  (:require [typed.clojure :as t]))
 
-(tc/ann add [tc/Int tc/Int :-> tc/Int])
+(t/ann add [t/Int t/Int :-> t/Int])
 (defn add
   "If you have https://github.com/Olical/typedclojure-lsp configured correctly you should see a type error / warning if you try to type (add :foo 10) inside this buffer."
   [a b]
   (+ a b))
+
+(comment
+  (add 10 :bar))
