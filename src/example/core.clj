6174cef104ca084f42d55c102b1e32d6db64f52b
Oliver Caldwell <ollie@oli.me.uk>
2025-06-21 13:06:46 +0100

    Remove core.clj example, not helpful

--- src/example/core.clj
+++
@@ -1,8 +0,0 @@
-(ns example.core
-  (:require [malli.experimental :as mx]))
-
-(mx/defn add :- number?
-  "If you have LSP configured correctly you should see a type error / warning if you try to type (add :foo 10) inside this buffer."
-  [a :- number?
-   b :- number?]
-  (+ a b))
--- test/example/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns example.core-test
-  (:require [clojure.test :as t]
-            [example.core :as ex]))
-
-(t/deftest add
-  (t/testing "it adds stuff"
-    (t/is (= 2 (ex/add 1 1)))))
